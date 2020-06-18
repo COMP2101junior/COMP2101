6aba9441abc7a730eeeb05f6eac1d47213e0dee2
Junior Kurian <juniorkurian63@gmail.com>
2020-06-18 08:21:54 -0700

    dice lab 2

---
+++ bash/diceroller.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# this script rolls a six-sided die
+#
+
+# Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
+
+# roll the dice and display the result
+echo "
+Rolling...
+$(( RANDOM % 6 + 1))-J
+$(( RANDOM % 6 + 1))-U
+$(( RANDOM % 6 + 1))-N
+$(( RANDOM % 6 + 1))-I
+$(( RANDOM % 6 + 1))-O
+$(( RANDOM % 6 + 1))-R
+"
