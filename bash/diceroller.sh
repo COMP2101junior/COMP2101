2f13fc9624e68f0115d2644a23fc85a268113778
Junior Kurian <juniorkurian63@gmail.com>
2020-06-18 08:38:18 -0700

    dice lab 2

--- bash/diceroller.sh
+++ bash/diceroller.sh
@@ -6,6 +6,7 @@
 # Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
 
 # roll the dice and display the result
+echo "rolling 6 times since i have 6 letters in my first name"
 echo "
 Rolling...
 $(( RANDOM % 6 + 1))-J
